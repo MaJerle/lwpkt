cf41acd6fef3a44b1cc736f7f9f4fb2c92cca198
Tilen Majerle <tilen.majerle@gmail.com>
2020-10-17 10:49:11 +0200

    Update project structure and main file

--- dev/VisualStudio/main.c
+++ dev/VisualStudio/main.c
@@ -17,12 +17,22 @@ main() {
     uint8_t b;
 
     /* Initialize packet and its buffers with default address */
-    lwpkt_init(&pkt, 0x12);
+    lwpkt_init(&pkt);
+#if LWPKT_CFG_USE_ADDR
+    lwpkt_set_addr(&pkt, 0x12);
+#endif /* LWPKT_CFG_USE_ADDR */
     lwrb_init(&pkt_tx_rb, pkt_tx_rb_data, sizeof(pkt_tx_rb_data));
     lwrb_init(&pkt_rx_rb, pkt_rx_rb_data, sizeof(pkt_rx_rb_data));
 
     /* Write packet data to TX ringbuff... */
-    res = lwpkt_write(&pkt, &pkt_tx_rb, 0x11, 0x85, data, strlen(data));
+    res = lwpkt_write(&pkt, &pkt_tx_rb,
+#if LWPKT_CFG_USE_ADDR
+        0x11,
+#endif /* LWPKT_CFG_USE_ADDR */
+#if LWPKT_CFG_USE_CMD
+        0x85,
+#endif /* LWPKT_CFG_USE_CMD */
+        data, strlen(data));
 
     /* Transmit TX buffer data to communication line... */
     /* Write received data from communication line to RX buffer */
@@ -42,9 +52,13 @@ main() {
         printf("Packet is valid!\r\n");
 
         /* Print debug messages for packet */
+#if LWPKT_CFG_USE_ADDR
         printf("Packet from: 0x%02X\r\n", (unsigned)lwpkt_get_from_addr(&pkt));
         printf("Packet to: 0x%02X\r\n", (unsigned)lwpkt_get_to_addr(&pkt));
+#endif /* LWPKT_CFG_USE_ADDR */
+#if LWPKT_CFG_USE_CMD
         printf("Packet cmd: 0x%02X\r\n", (unsigned)lwpkt_get_cmd(&pkt));
+#endif /* LWPKT_CFG_USE_CMD */
         printf("Packet data length: 0x%02X\r\n", (unsigned)lwpkt_get_data_len(&pkt));
         if ((len = lwpkt_get_data_len(&pkt)) > 0) {
             uint8_t* d = lwpkt_get_data(&pkt);
@@ -56,6 +70,7 @@ main() {
         }
 
         /* Check who should be dedicated receiver */
+#if LWPKT_CFG_USE_ADDR
         if (lwpkt_is_for_me(&pkt)) {
             printf("Packet is for me\r\n");
         } else if (lwpkt_is_broadcast(&pkt)) {
@@ -63,6 +78,7 @@ main() {
         } else {
             printf("Packet is for device ID: 0x%02X\r\n", (unsigned)lwpkt_get_to_addr(&pkt));
         }
+#endif /* LWPKT_CFG_USE_ADDR */
     } else {
         printf("Packet is not valid!\r\n");
     }
