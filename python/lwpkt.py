fc4772d3f8e76d48d3021aa406bd62007fb336f5
Tilen Majerle <tilen.majerle@gmail.com>
2024-03-14 20:01:52 +0100

    Add more, work to be done

--- python/lwpkt.py
+++ python/lwpkt.py
@@ -1,4 +1,4 @@
-import enum, struct
+import enum, struct, queue
 
 '''
 This file is currently in the development 
@@ -6,6 +6,29 @@ and shall not be used by the final application
 '''
 
 class LwPKT(object):
+    class LwPKT_Receive(object):
+
+        class State(enum.Enum):
+            START  = 0
+            FROM   = 1
+            TO     = 2
+            CMD    = 3     
+            FLAGS  = 4     
+            LEN    = 5    
+            DATA   = 6  
+            CRC    = 7
+            STOP   = 8 
+            END    = 9
+
+        def __init__(self):
+            self.data_raw = bytearray()
+            self.state = LwPKT.LwPKT_Receive.State.START
+            self.len = 0
+            self.data = bytearray()
+            self.flags = 0
+            self.cmd = 0
+            self.crc = 0
+            self.index = 0
 
     '''
     LwPKT object
@@ -17,7 +40,9 @@ class LwPKT(object):
         self.opt_crc = True
         self.opt_crc32 = True
         self.opt_flags = True
-        self.our_addr = 0x12
+        self.our_addr = 0
+        self.rx_data = queue.Queue()
+        self.rx = LwPKT.LwPKT_Receive()
 
     # Generate the packet that can be sent out
     def generate_packet(self, data:bytes, cmd:int = 0, addr_to:int = 0, flags:int = 0) -> bytes:
@@ -86,6 +111,24 @@ class LwPKT(object):
                 crc_old_val = crc_old_val ^ (0xEDB88320 if self.opt_crc32 else 0x8C)
             new_byt = new_byt >> 1
         return crc_old_val
+    
+    # Write RX data to packet 
+    def write_rx_data(self, data:bytearray):
+        for d in data: self.rx_data.put_nowait(d)
+
+    # Process RX data
+    # TODO: Implement
+    def process_rx_data(self):
+        ret = False
+        while not self.rx_data.empty():
+            ch = self.rx_data.get_nowait()
+
+            if self.rx.state == LwPKT.LwPKT_Receive.State.START:
+                if ch == 0xAA:
+                    self.rx.state = LwPKT.LwPKT_Receive.State.FROM
+            elif self.rx.state == LwPKT.LwPKT_Receive.State.FROM:
+                pass
+        return ret
 
 if __name__ == '__main__':
     print('main')
@@ -100,6 +143,12 @@ if __name__ == '__main__':
     pkt.opt_flags = True
     pkt.opt_crc = True
 
+    # Encode
     packet = pkt.generate_packet(data, addr_to = 0x87654321, flags = 0xACCE550F, cmd=0x85)
     print('packet', len(packet))
     print('packet_data', ', '.join(['0x{:02X}'.format(i) for i in packet]))
+
+    # Decode
+    pkt.write_rx_data(packet)
+    decoded = pkt.process_rx_data()
+    print('decoded', decoded)
